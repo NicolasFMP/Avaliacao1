ffa98ed71ac3d2a2822e2c013f7bb5348b24fe2d
Nicolas <nicolas.costa@aluno.fmpsc.edu.br>
2026-09-17 14:11:41 -0300

    aaaaaaa

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
-void mostrarMenu();
+void exibirMenu();
 void cadastro(char nomes[][50], float notas[], int *qtdAluno);
-void exibirAlunos(char nomes[][50], int *qtdAluno);
+void exibirAlunos(char nomes[][50],float notas[], int *qtdAluno);
+void exibirMediaTurma(float notas[],int *qtdAluno);
+void exibirAlunosAprovados(char nomes[][50], float notas[], int *qtdAluno);
 
 int main() {
     char nomes[100][50];
@@ -12,7 +15,7 @@ int main() {
     int escolha;
 
     do {
-        mostrarMenu();
+        exibirMenu();
         scanf("%d", &escolha);
 
         switch (escolha) {
@@ -20,15 +23,25 @@ int main() {
                 cadastro(nomes, notas, &qtdAluno);
                 break;
             case 2:
-                exibirAlunos(nomes, &qtdAluno);
+                exibirAlunos(nomes, notas, &qtdAluno);
                 break;
+            case 3:
+                exibirMediaTurma(notas, &qtdAluno);
+                break;
+            case 4:
+                exibirAlunosAprovados(nomes, notas, &qtdAluno);
+                break;
+            default:
+                if (escolha != 0) {
+                    printf("Opcao invalida\n");
+                }
         }
     }while (escolha != 0);
 
 }
 
-void mostrarMenu() {
-    printf("1. Cadastrar aluno e notas\n"
+void exibirMenu() {
+    printf("\n1. Cadastrar aluno e notas\n"
            "2. Exibir alunos\n"
            "3. Calcular e exibir media geral da turma\n"
            "4. Exibir alunos aprovados (Média >= 7.0)\n"
@@ -39,7 +52,9 @@ void mostrarMenu() {
 void cadastro(char nomes[][50], float notas[], int *qtdAluno) {
     printf("Digite o nome do aluno: ");
     while (getchar() != '\n');
+
     fgets(nomes[*qtdAluno], 50, stdin);
+    nomes[*qtdAluno][strcspn(nomes[*qtdAluno], "\n")] = '\0';
 
     printf("Digite a nota do aluno: ");
     scanf("%f", &notas[*qtdAluno]);
@@ -47,8 +62,32 @@ void cadastro(char nomes[][50], float notas[], int *qtdAluno) {
     (*qtdAluno)++;
 }
 
-void exibirAlunos(char nomes[][50], int *qtdAluno) {
+void exibirAlunos(char nomes[][50],float notas[], int *qtdAluno) {
     for (int i = 0; i < *qtdAluno; i++) {
-        printf("Aluno %d: %s\n", i + 1, nomes[i]);
+        printf("Aluno %d: %s, %.2f\n", i + 1, nomes[i], notas[i]);
+    }
+}
+
+void exibirMediaTurma(float notas[],int *qtdAluno) {
+    if (*qtdAluno == 0) {
+        printf("Nenhum aluno cadastrado\n");
+        return;
+    }
+
+    float soma = 0.0;
+    
+    for (int i = 0; i < *qtdAluno; i++) {
+        soma += notas[i];
+    }
+
+    printf("Media da turma: %.2f", soma / *qtdAluno);
+}
+
+void exibirAlunosAprovados(char nomes[][50], float notas[], int *qtdAluno) {
+    printf("Alunos aprovados:\n");
+    for (int i = 0; i < *qtdAluno; i++) {
+        if (notas[i] >= 7.0) {
+            printf("%s: %.2f\n", nomes[i], notas[i]);
+        }
     }
 }
