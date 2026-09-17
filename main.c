1a6b54cd227bbcbbb76b1c827fb287765af42cb0
Nicolas <nicolas.costa@aluno.fmpsc.edu.br>
2026-09-17 10:56:48 -0300

    Atualizacao

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 
 void mostrarMenu();
 void cadastro(char nomes[][50], float notas[], int *qtdAluno);
+void exibirAlunos(char nomes[][50], int *qtdAluno);
 
 int main() {
     char nomes[100][50];
     float notas[100];
+    int qtdAluno = 0;
 
     int escolha;
 
@@ -15,7 +17,11 @@ int main() {
 
         switch (escolha) {
             case 1:
-                
+                cadastro(nomes, notas, &qtdAluno);
+                break;
+            case 2:
+                exibirAlunos(nomes, &qtdAluno);
+                break;
         }
     }while (escolha != 0);
 
@@ -23,13 +29,26 @@ int main() {
 
 void mostrarMenu() {
     printf("1. Cadastrar aluno e notas\n"
-           "3. Exibir alunos\n"
-           "4. Calcular e exibir media geral da turma\n"
-           "5. Exibir alunos aprovados (Média >= 7.0)\n");
+           "2. Exibir alunos\n"
+           "3. Calcular e exibir media geral da turma\n"
+           "4. Exibir alunos aprovados (Média >= 7.0)\n"
+           "0. Sair\n");
     printf("Opção: ");
 }
 
 void cadastro(char nomes[][50], float notas[], int *qtdAluno) {
     printf("Digite o nome do aluno: ");
+    while (getchar() != '\n');
     fgets(nomes[*qtdAluno], 50, stdin);
+
+    printf("Digite a nota do aluno: ");
+    scanf("%f", &notas[*qtdAluno]);
+
+    (*qtdAluno)++;
+}
+
+void exibirAlunos(char nomes[][50], int *qtdAluno) {
+    for (int i = 0; i < *qtdAluno; i++) {
+        printf("Aluno %d: %s\n", i + 1, nomes[i]);
+    }
 }
